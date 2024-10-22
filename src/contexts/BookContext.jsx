ca77707ee1e00c70a5e409ef0a669a2f8b85eb5d
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2024-10-22 23:49:46 +0100

    useContext

---
+++ src/contexts/BookContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+import { v4 as uuidv10 } from "uuid"; // Updated import for uuidv4
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+  const [books, setBooks] = useState([
+    {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
+    {title: 'the final empire', author: 'brandon sanderson', id: 2},
+  ]);
+  const addBook = (title, author) => {
+    setBooks([...books, {title, author, id: uuidv10()}]);
+  };
+  const removeBook = (id) => {
+    setBooks(books.filter(book => book.id !== id));
+  }
+
+  return (
+    <BookContext.Provider value={{ books, addBook, removeBook }}>
+      {props.children}
+    </BookContext.Provider>
+  );
+}
+ 
+export default BookContextProvider;
